9ff2ad434840cb95aeb607eb80534b613aaa962d
Raul Rodriguez Proenza <rproenza86@gmail.com>
2016-07-16 23:15:58 -0400

    Ended level 1

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -18,7 +18,7 @@ Instructions:
    * Helper function to show the search query.
    * @param {String} response - The unparsed JSON response from get.
    */
-  function addSearchHeader(response) {
+  function addSearchHeader(response) {//console.log(response);
     try {
       response = JSON.parse(response).query;  // you'll be moving this line out of here in the next quiz!
     } catch (e) {
@@ -33,34 +33,51 @@ Instructions:
    * @return {Promise}    - A Promise that resolves when the XHR succeeds and fails otherwise.
    */
   function get(url) {
-    /*
-    This code needs to get wrapped in a Promise!
-     */
-    var req = new XMLHttpRequest();
-    req.open('GET', url);
-    req.onload = function() {
-      if (req.status === 200) {
-        // It worked!
-        // You'll want to resolve with the data from req.response
-      } else {
-        // It failed :(
-        // Be nice and reject with req.statusText
-      }
-    };
-    req.onerror = function() {
-      // It failed :(
-      // Pass a 'Network Error' to reject
-    };
-    req.send();
+    /*return new Promise(function(resolve, reject) {
+        var req = new XMLHttpRequest();
+        req.open('GET', url);
+        req.onload = function() {
+          if (req.status === 200) {
+            resolve(req.response);
+          } else {
+            reject(Error(req.statusText));
+          }
+        };
+        req.onerror = function() {
+          reject(Error('Network Error'));
+        };
+        req.send();
+    });*/
+    return fetch(url);
   }
+
+
+    /**
+     * Perform an XHR for a JSON and returns a parsed JSON response.
+     * @param  {String} url - The JSON URL to fetch.
+     * @return {Promise}    - A Promise that passes the parsed JSON.
+     */
+    function getJSON(url) {
+      return get(url).then(function(response) {
+            	 return response.json();
+            });
+    }
 
   window.addEventListener('WebComponentsReady', function() {
     home = document.querySelector('section[data-route="home"]');
-    /*
-    Uncomment the next line you're ready to start chaining and testing!
-    You'll need to add a .then and a .catch. Pass the response to addSearchHeader on resolve or
-    pass 'unknown' to addSearchHeader if it rejects.
-     */
-    // get('../data/earth-like-results.json')
+
+    getJSON('../data/earth-like-results.json')
+     .then( function (response) {
+       addSearchHeader(response.query);
+       console.debug(response);
+       return response.results[0];
+     })
+     .then(function (url) {
+       console.log(url);
+     })
+     .catch(function (error){
+       addSearchHeader('unknown');
+       console.log(error);
+     });
   });
 })(document);
